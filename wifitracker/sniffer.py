7e08a3e0ceba0d50943d66db86101fd1ea727af7
David Url <david@x00.at>
2014-10-21 14:42:16 +0200

    fixed sniffing and logging

--- wifitracker/sniffer.py
+++ wifitracker/sniffer.py
@@ -1,6 +1,9 @@
+import logging
+
 from scapy.all import Dot11, Dot11ProbeReq
 from scapy.all import sniff as scapysniff
 
+log = logging.getLogger(__name__)
 
 PR_TYPE = 0
 PR_SUBTYPE = 4
@@ -8,9 +11,10 @@ PR_SUBTYPE = 4
 
 def _extract_signal_strength(packet):
     try:
-        extra = packet.notecoded
+        extra = packet.notdecoded
         signal_strength = -(256 - ord(extra[-4:-3]))
-    except:
+    except Exception as e:
+        print e
         signal_strength = None
     return signal_strength
 
@@ -20,20 +24,23 @@ class ProbeRequest(object):
     def __init__(self, packet):
         self.target = packet.addr3
         self.source = packet.addr2
-        self.target_ssid = packet.getlayer(Dot11ProbeReq).info
         self.signal_strength = _extract_signal_strength(packet)
+        self.source_ssid = None
+        ssid = packet.getlayer(Dot11ProbeReq).info
+        if len(ssid) > 0:
+            self.source_ssid = ssid
 
     def __str__(self):
-        return "Source: {} SSID: {} RSSi: {}".format(self.source,
-                                                     self.target_ssid,
-                                                     self.signal_strength)
+        return "Source: {} || SSID: {} || RSSi: {}".format(self.source,
+                                                           self.target_ssid,
+                                                           self.signal_strength)
 
 
 def packet_handler(packet):
     if packet.haslayer(Dot11):
         if (packet.type == PR_TYPE and packet.subtype == PR_SUBTYPE):
             request = ProbeRequest(packet)
-            print request
+            log.info(request)
 
 
 def sniff(iface):
